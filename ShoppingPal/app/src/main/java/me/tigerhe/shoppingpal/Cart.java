296e6253729e589ca81b6b0d8a8d600d4d9a0c1d
Tiger He <tigerhe7@gmail.com>
2016-11-12 13:37:06 -0500

    Added touch listener that doens't work

--- ShoppingPal/app/src/main/java/me/tigerhe/shoppingpal/Cart.java
+++ ShoppingPal/app/src/main/java/me/tigerhe/shoppingpal/Cart.java
@@ -1,13 +1,34 @@
 package me.tigerhe.shoppingpal;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class Cart extends AppCompatActivity {
+
+    OnSwipeTouchListener onSwipeTouchListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
+
+        onSwipeTouchListener = new OnSwipeTouchListener(Cart.this) {
+            @Override
+            public void onSwipeLeft() {
+                System.exit(0);
+                switchToCamera();
+            }
+        };
+
+        // onSwipeTouchListener.onSwipeLeft();
+
     }
+
+    public void switchToCamera() {
+        Intent intent = new Intent(this, Camera.class);
+        startActivity(intent);
+    }
+
+
 }
