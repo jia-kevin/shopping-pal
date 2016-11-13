eb0234aa8280021282d82fa16d471f6c54c77e66
jia-kevin <jia.kevin42@gmail.com>
2016-11-13 03:22:26 -0500

    send barcode to cart

--- ShoppingPal/app/src/main/java/me/tigerhe/shoppingpal/Cart.java
+++ ShoppingPal/app/src/main/java/me/tigerhe/shoppingpal/Cart.java
@@ -3,9 +3,13 @@ package me.tigerhe.shoppingpal;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
+
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.vision.barcode.Barcode;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +24,8 @@ public class Cart extends AppCompatActivity {
     // list view and adapter for data
     private ListView mList;
     private ItemListAdapter mAdapter;
+    final int RC_BARCODE_CAPTURE = 9001;
+    int barcodeValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,10 +52,32 @@ public class Cart extends AppCompatActivity {
     public void switchToCamera() {
         mProductList.add("wow");
         mAdapter.updateList(mProductList);
-
         Intent intent = new Intent(this, BarcodeCaptureActivity.class);
-        startActivity(intent);
+        startActivityForResult(intent, RC_BARCODE_CAPTURE);
     }
 
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RC_BARCODE_CAPTURE) {
+            if (resultCode == CommonStatusCodes.SUCCESS) {
+                if (data != null) {
+                    Barcode barcode = data.getParcelableExtra(BarcodeCaptureActivity.BarcodeObject);
 
+                    //barcodeValue = Integer.parseInt(barcode.displayValue);
+                }
+            }
+        }
+        else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+/*
+    public void sendMessage() {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.input_task);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+    */
 }
