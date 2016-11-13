0fdf9782811d4363ac9eaa930ce33edeb165b2d2
kzzhang <kzhang2100@gmail.com>
2016-11-13 06:54:40 -0500

    Revert "Revert "Revert "Updated Product Class"""
    This reverts commit 2d0f9e733e049fee7b14218f4fa571ee09f65d7f.

--- ShoppingPal/app/src/main/java/me/tigerhe/shoppingpal/AmazonProduct.java
+++ ShoppingPal/app/src/main/java/me/tigerhe/shoppingpal/AmazonProduct.java
@@ -17,7 +17,7 @@ public class AmazonProduct {
     public AmazonProduct(){
     }
 
-    public static void setXml(String XmlInput){
+    public AmazonProduct(String XmlInput){
         int index1, index2;
         //for name
         index1 = XmlInput.indexOf("<Title>")+7;
